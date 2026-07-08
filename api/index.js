ce8146ff196e6a7120b059bef8af8505136be8a2
apnunka <biodocytube@gmail.com>
2026-07-08 21:03:31 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,50 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+module.exports = (req, res) => {
+    // Prevents security blocks from the browser
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-const app = express();
-app.use(cors());
-
-// 1. The Manifest Route
-app.get('/api/manifest.json', (req, res) => {
-    res.json({
-        id: 'org.myaddon.cinestream',
-        version: '1.0.0',
-        name: 'CineStream Direct',
-        description: 'Direct Web Streams for Nuvio TV',
-        resources: ['stream'],
-        types: ['movie'],
-        idPrefixes: ['tt']
-    });
-});
-
-// 2. The Video Stream Route
-app.get('/api/stream/:type/:id.json', async (req, res) => {
-    const { type, id } = req.params;
-    
-    if (type === 'movie' && id.startsWith('tt')) {
-        try {
-            // Get the movie name
-            const metaRes = await axios.get(`https://v3-cinemeta.strem.io/meta/movie/${id}.json`);
-            const title = metaRes.data?.meta?.name || 'Movie';
-            
-            // Set the web player link
-            const embedUrl = `https://vidsrc.me/embed/movie?imdb=${id}`;
-
-            return res.json({
-                streams: [
-                    {
-                        name: 'CineStream',
-                        title: `${title}\n1080p | Direct Web Player`,
-                        externalUrl: embedUrl
-                    }
-                ]
-            });
-        } catch (error) {
-            return res.json({ streams: [] });
-        }
+    // 1. Hands Nuvio the Manifest
+    if (req.url.includes('/manifest.json')) {
+        return res.json({
+            id: 'org.myaddon.cinestream',
+            version: '1.0.0',
+            name: 'CineStream Direct',
+            description: 'Direct Web Streams',
+            resources: ['stream'],
+            types: ['movie'],
+            idPrefixes: ['tt']
+        });
     }
-    return res.json({ streams: [] });
-});
 
-module.exports = app;
+    // 2. Hands Nuvio the Video Link
+    if (req.url.includes('/stream/movie/')) {
+        const id = req.url.split('/').pop().replace('.json', '');
+        return res.json({
+            streams: [{
+                name: 'CineStream',
+                title: '1080p | Direct Web',
+                externalUrl: `https://vidsrc.me/embed/movie?imdb=${id}`
+            }]
+        });
+    }
+
+    // 3. Catches any other random traffic
+    return res.status(404).send('Not Found');
+};
